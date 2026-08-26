da24f8e79461c82a2484a19b917f557b9f8e244a
komalll6 <komalkalra698@gmail.com>
2026-08-26 14:31:52 +0530

    FAST-API with query  parameter- commit

--- param.py
+++ param.py
@@ -5,7 +5,7 @@ app=FastAPI()
 #route
 #path parameter- if we dont know the id
 
-@app.get("/students/(student_id)") 
+@app.get("/students/{student_id}") 
 def get_studentbyid(student_id: int):
     return{
         "student details":  "student_id"
---
+++ queryparam.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/queryparam")
+def get_query(name:str, age:int):
+    return{
+        "name":name,
+        "age":age
+    }
+
+
+#http://127.0.0.1:8000/queryparam?name=koma
+#http://127.0.0.1:8000/queryparam?name=komal&age=22
