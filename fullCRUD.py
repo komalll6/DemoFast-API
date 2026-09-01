709c2d6e231384032cb04e6f6b0d24e40e390c6e
komalll6 <komalkalra698@gmail.com>
2026-09-01 18:28:42 +0530

    another commit with all methods - CRUD

--- fullCRUD.py
+++ fullCRUD.py
@@ -38,6 +38,7 @@ students = [
 }
 ]
 
+
 #get all students => GET METHOD
 @app.get("/students")
 def get_students():
@@ -53,6 +54,29 @@ def get_student(student_id: int):
     return {"message": "Student not found"}
 
 
+#create method => with post method
+@app.post("/students")
+def create_student(students: Student):
+    new_student={
+        "id":len(students)+1,
+        **students.model_dump()
+    }
+    students.append(new_student)
+    return new_student
+
+
+#create update method => with put method
+@app.put("/students/{student_id}")
+def update_student(student_id: int, updated_student: Student):
+    for existing_student in students:
+        if existing_student["id"] == student_id:
+            existing_student.update(updated_student.model_dump())
+            return existing_student
+        
+    return{"message": "Student not found"}
+
+
+
 #delete student by id => DELETE METHOD
 @app.delete("/students/{student_id}")
 def delete_student(student_id: int):
@@ -61,7 +85,3 @@ def delete_student(student_id: int):
             students.pop(index) #pop = helps to remove the element from the list
             return {"message": "Student deleted successfully"}
     return {"message": "Student not found"}
-
-
-
-#put => to update the student details
