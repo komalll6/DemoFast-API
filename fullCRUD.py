0c688d8ca3009ae1b30226e0f632905f9af60d39
komalll6 <komalkalra698@gmail.com>
2026-09-01 14:52:13 +0530

    another commit with FullCRUD FASTapi

--- fullCRUD.py
+++ fullCRUD.py
@@ -1,18 +1,21 @@
 #to remove - use delete method
 #index mei apne ap remove krdega => enumerate
+#mongodb atlas =>online cloud based ( fastapi + mongodb atlas) => CRUD operations
+#mongodb are noSQL databases
+
+#pip install pymongo => hv mongodb client(funcion give connection string)
 
 from fastapi import FastAPI
 
 from pydantic import BaseModel
 
+app= FastAPI()
 
 #temp storage
 class Student(BaseModel):
     name:str
     age:int
     course:str
-
-app= FastAPI()
 
 students = [
 {
@@ -38,7 +41,7 @@ students = [
 #delete student by id => DELETE METHOD
 @app.delete("/students/{student_id}")
 def delete_student(student_id: int):
-    for index, student in enumerate(students):
+    for index, student in enumerate(students): #needfor index => use enumerate => to get the index of the student in the list
         if student["id"] == student_id:
             students.pop(index) #pop = helps to remove the element from the list
             return {"message": "Student deleted successfully"}
